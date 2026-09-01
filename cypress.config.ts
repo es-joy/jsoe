eeca5efbe9058fb3c9958f1c45cba621602dff88
Brett Zamir <brettz9@yahoo.com>
2026-09-01 11:10:11 -0700

    chore: update cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,10 +2,8 @@ import {defineConfig} from 'cypress';
 import plugins from './cypress/plugins/index.js';
 
 export default defineConfig({
-  allowCypressEnv: false,
   defaultCommandTimeout: 8000,
   video: false,
-  experimentalMemoryManagement: true,
   numTestsKeptInMemory: 1, // 50
   projectId: 'biz82n', // Cypress Cloud
   e2e: {
